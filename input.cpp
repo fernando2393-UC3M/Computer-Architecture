c655f25dbae568b82b937b5f070c18145806c09e
fernando2393 <garciasanzfernando2393@gmail.com>
2017-11-19 19:37:41 +0100

    Input done

---
+++ input.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int num_asteroids = atoi(argv[1]);
+  int num_iterations = atoi(argv[2]);
+  int num_planets = atoi(argv[3]);
+  double pos_ray = atoi(argv[4]);
+  int seed = atoi(argv[5]);
+
+  if(num_asteroids<0){
+    cerr << "Number of asteroids is lower than zero!" << endl;
+    return 0;
+  }
+
+  if(num_iterations<0){
+    cerr << "Number of iterations is lower than zero!" << endl;
+    return 0;
+  }
+
+  if(num_planets<0){
+    cerr << "Number of planets is lower than zero!" << endl;
+    return 0;
+  }
+
+  if(seed == 0 || seed <0){
+    cerr << "Seed is equal or lower than zero!" << endl;
+    return 0;
+  }
+
+  ofstream myFile ("init_conf.txt");
+  if(myFile.is_open()){
+    myFile << num_asteroids << " ";
+    myFile << num_iterations << " ";
+    myFile << num_planets << " ";
+    myFile << pos_ray << " ";
+    myFile << seed;
+  }
+
+  else{
+    cerr << "Error creating the file!" << endl;
+  }
+
+  return 0;
+}
