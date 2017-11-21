fee0dc8b5410c1070b9b5fcb0301542976e0d98b
JavaLavadora <tonidiazmartinez1997@gmail.com>
2017-11-21 22:28:42 +0100

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -14,36 +14,66 @@ const double HEIGHT = 200;
 const double RAY_WIDTH = 4;
 const double MASS = 1000;
 const double SDM = 50;
+const double MAXFORCE = 200;
+
 
 
 struct asteroids{
-  double x, y, mass;
+  double x, y, vx,vy, ax, ay, mass;
 };
 
 struct planets{
-  double x, y;
+  double x, y, mass;
 };
 
-void random(const int seed, int num_asteroids, int num_planets, struct asteroids ast[], struct planets pl[], double ray_x, double ray_y){
-  std::default_random_engine re{seed};
-  std::uniform_real_distribution<double> xdist{0.0, std::nextafter(WIDTH, std::numeric_limits<double>::max())};
-  std::uniform_real_distribution<double> ydist{0.0, std::nextafter(HEIGHT, std::numeric_limits<double>::max())};
-  std::normal_distribution<double> mdist{MASS, SDM};
 
-  for (int i = 0; i < num_asteroids; i++) {
-    ast[i].x = xdist(re);
-    ast[i].y = ydist(re);
-    ast[i].mass = mdist(re);
+double computeDistance(double xa, double ya, double xb, double yb){
+  double distance;
+
+  distance=pow(xa-xb,2)+pow(ya-yb,2);
+  distance=sqrt(distance);
+printf("DISTANCE: %f \n", distance);
+  return distance;
+}
+
+double computeAngle(double xa, double ya, double xb, double yb){
+  double slope;
+  double angle;
+
+  slope=(ya-yb)/(xa-xb);
+  //Correction for the slope
+  if(slope>1 || slope<-1){
+    slope=slope-trunc(slope);
   }
+  angle= atan(slope);
 
-  for (int i = 0; i < num_planets; i++) {
-    pl[i].x = xdist(re);
-    pl[i].y = ydist(re);
+  return angle;
+}
+
+double computeForceX(double ma, double mb, double dist, double ang){
+  double f;
+
+  f=GRAVITY*ma*mb;
+  f=f/dist;
+  f=f*cos(ang);
+
+  if(f>MAXFORCE){
+    f=200; //Truncate to 200 if maxforce is surpassed
   }
+  return f;
+}
 
-  ray_x = xdist(re);
-  ray_y = ydist(re);
+double computeForceY(double ma, double mb, double dist, double ang){
+  double f;
 
+  f=GRAVITY*ma*mb;
+  f=f/dist;
+  f=f*sin(ang);
+
+  if(f>MAXFORCE){
+    f=200; //Truncate to 200 if maxforce is surpassed
+  }
+  return f;
 }
 
 int main(int argc, char const *argv[]) {
@@ -53,6 +83,7 @@ int main(int argc, char const *argv[]) {
   double pos_ray = atoi(argv[4]);
   const int seed = atoi(argv[5]);
 
+  //ARGUMENTS CHECK
   if(num_asteroids<0){
     cerr << "nasteroids-seq: Wrong arguments." << endl;
     cerr << "Correct use:" << endl;
@@ -83,12 +114,42 @@ int main(int argc, char const *argv[]) {
 
   asteroids ast[num_asteroids];
   planets pl[num_planets];
+  //random(seed, num_asteroids, num_planets, ast, pl, ray_x, ray_y);
 
-  double ray_x;
-  double ray_y;
+  //JUST SOME TESTS UNTIL RANDOM WORKS
+  for (int i = 0; i < num_asteroids; i++) {
+    ast[0].x = 1;
+    ast[0].y = 0;
+    ast[0].mass = 10;
+    ast[0].vx = 0;
+    ast[0].vy = 0;
+    ast[0].ax = 0;
+    ast[0].ay = 0;
 
-  random(seed, num_asteroids, num_planets, ast, pl, ray_x, ray_y);
+    ast[1].x = 2;
+    ast[1].y = 0;
+    ast[1].mass = 20;
+    ast[1].vx = 0;
+    ast[1].vy = 0;
+    ast[1].ax = 0;
+    ast[1].ay = 0;
 
+    ast[2].x = 1;
+    ast[2].y = 2;
+    ast[2].mass = 30;
+    ast[2].vx = 0;
+    ast[2].vy = 0;
+    ast[2].ax = 0;
+    ast[2].ay = 0;
+  }
+
+  for (int i = 0; i < num_planets; i++) {
+    pl[i].x = 0;
+    pl[i].y = 100;
+  }
+
+
+  //INITIAL DATA FILE
   ofstream myFile ("init_conf.txt");
   if(myFile.is_open()){
     myFile << num_asteroids << " ";
@@ -107,34 +168,99 @@ int main(int argc, char const *argv[]) {
       myFile << pl[i].x << " ";
       myFile << pl[i].y << endl;
     }
-
-    myFile << ray_x << " ";
-    myFile << ray_y << endl;
-
+    myFile << pos_ray << endl;
+  }
+  else{
+    cerr << "Error creating the input file!" << endl;
   }
 
+  double distMatrix[num_asteroids][num_asteroids+num_planets]; //Distance of body i with respect to body j
+  double angleMatrix[num_asteroids][num_asteroids+num_planets]; //Angle of body i with respect to body j
+  double forcesMatrixX[num_asteroids][num_asteroids+num_planets]; //Force X of body i with respect to body j
+  double forcesMatrixY[num_asteroids][num_asteroids+num_planets]; //Force X of body i with respect to body j
+
+  //SIMULATION
+  for(int t=0; t<num_iterations; t++){
+    for(int i=0; i< num_asteroids; i++){
+      for(int j=0; j< num_asteroids+num_planets; j++){
+        printf("ITERATION: i=%i  j=%i\n", i, j);
+        if(i!=j){ //Avoid evaluate i with itself
+          if(i<num_asteroids){ //ASTEROID
+            //Dist
+            distMatrix[i][j]=computeDistance(ast[i].x, ast[i].y, ast[j].x, ast[j].y);
+            if(distMatrix[i][j]<DMIN){
+              //Angle
+              angleMatrix[i][j]=computeAngle(ast[i].x, ast[i].y, ast[j].x, ast[j].y);
+              //Force
+              if(forcesMatrixX==0 && forcesMatrixY==0){ //Check if it has not been previously set
+                forcesMatrixX[i][j]=computeForceX(ast[i].mass, ast[j].mass, distMatrix[i][j], angleMatrix[i][j]);
+                forcesMatrixY[i][j]=computeForceY(ast[i].mass, ast[j].mass, distMatrix[i][j], angleMatrix[i][j]);
+                //Attraction force for second asteroid
+                forcesMatrixX[j][i]=forcesMatrixX[i][j]*(-1);
+                forcesMatrixY[j][i]=forcesMatrixX[i][j]*(-1);
+              }
+            }
+          }
+
+          else{ //PLANET
+            //Dist
+            distMatrix[i][j]=computeDistance(ast[i].x, ast[i].y, pl[j].x, pl[j].y);
+            if(distMatrix[i][j]>DMIN){
+              //Angle
+              angleMatrix[i][j]=computeAngle(ast[i].x, ast[i].y, pl[j].x, pl[j].y);
+              //Force
+              forcesMatrixX[i][j]=computeForceX(ast[i].mass, pl[j].mass, distMatrix[i][j], angleMatrix[i][j]);
+              forcesMatrixY[i][j]=computeForceY(ast[i].mass, pl[j].mass, distMatrix[i][j], angleMatrix[i][j]);
+            }
+          }
+        }
+      }
+    }
+
+    //Results should be updated once the matrix is filled as if not the i+1 asteroid in t would be taking into account the t+1 position of the i asteroid
+    //Rebound(destance check + velocity) should be computed when the asteroid is about to move
+
+
+  }
+  //OUTPUT DATA
+  ofstream myFile2 ("out.txt");
+  if(myFile2.is_open()){
+    for(int i = 0; i < num_asteroids; i++){
+      myFile << ast[i].x << " ";
+      myFile << ast[i].y << " ";
+      myFile << ast[i].vx << " ";
+      myFile << ast[i].vy << " ";
+      myFile << ast[i].mass << endl;
+    }
+  }
   else{
     cerr << "Error creating the file!" << endl;
   }
 
-  //-------------------------HERE STARTS THE COMPUTATION OF FORCES//-------------------------//
-
-  //Here we need to create an array attraction of size num_asteroids+num_planets-1
-
-  for(int i = 0; i < num_asteroids; i++){
-    for(int j = 0; j < (num_asteroids+num_planets-1); j++){
-      if(j!=i){
-        if(j<num_asteroids){
-          ast[i].attraction[j] = sqrt(pow(ast[i].x-ast[j].x,2)+pow(ast[i].y-ast[j].y,2));
-        }
-        else{
-          ast[i].attraction[j] = sqrt(pow(ast[i].x-pl[j-(num_asteroids-1)].x,2)+pow(ast[i].y-pl[j-(num_asteroids-1)].y,2));
-        }
-      }
-    }
-  }
-
-
 
   return 0;
 }
+
+/*RANDOM GENERATION
+void random(const int seed, int num_asteroids, int num_planets, struct asteroids ast[], struct planets pl[], double ray_x, double ray_y){
+  std::default_random_engine re{seed};
+  std::uniform_real_distribution<double> xdist{0.0, std::nextafter(WIDTH, std::numeric_limits<double>::max())};
+  std::uniform_real_distribution<double> ydist{0.0, std::nextafter(HEIGHT, std::numeric_limits<double>::max())};
+  std::normal_distribution<double> mdist{MASS, SDM};
+
+  for (int i = 0; i < num_asteroids; i++) {
+    ast[i].x = xdist(re);
+    ast[i].y = ydist(re);
+    ast[i].mass = mdist(re);
+  }
+
+  for (int i = 0; i < num_planets; i++) {
+    pl[i].x = xdist(re);
+    pl[i].y = ydist(re);
+  }
+
+  ray_x = xdist(re);
+  ray_y = ydist(re);
+
+}
+*/
